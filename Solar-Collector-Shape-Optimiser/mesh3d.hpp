b5474e26660c18301d4fce6dcb212b53e70a2d72
xBartM <mielczarek.cs@gmail.com>
2025-01-27 15:00:25 +0100

    merged triange.hpp and vertex.hpp to consolidate all mesh functions

--- Solar-Collector-Shape-Optimiser/mesh3d.hpp
+++ Solar-Collector-Shape-Optimiser/mesh3d.hpp
@@ -1,7 +1,15 @@
-#ifndef TRIANGLE_HPP
-#define TRIANGLE_HPP
+#ifndef MESH3D_HPP
+#define MESH3D_HPP
 
-#include <Solar-Collector-Shape-Optimiser/vertex.hpp>
+
+struct vertex {
+    double x;
+    double y;
+    double z;
+
+    vertex() : x(0), y(0), z(0) {};
+    vertex(double xx, double yy, double zz) : x(xx), y(yy), z(zz) {};
+};
 
 struct triangle {
     vertex normal;
@@ -11,4 +19,4 @@ struct triangle {
     triangle(vertex n, vertex v1, vertex v2, vertex v3) : normal(n), v{v1, v2, v3} {};
 };
 
-#endif
+#endif // MESH3D
